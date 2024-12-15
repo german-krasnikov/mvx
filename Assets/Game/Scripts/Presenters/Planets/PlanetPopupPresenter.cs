e70090e33761c6ec86c27bd7b78ba95b25c151e1
German Krasnikov <german.krasnikov@gmail.com>
2024-12-15 20:11:30 +0100

    progress

--- Assets/Game/Scripts/Presenters/Planets/PlanetPopupPresenter.cs
+++ Assets/Game/Scripts/Presenters/Planets/PlanetPopupPresenter.cs
@@ -1,5 +1,6 @@
 using System;
 using Game.Views.Planets;
+using Modules.Money;
 using Modules.Planets;
 using UnityEngine;
 using Zenject;
@@ -14,15 +15,18 @@ namespace Game.Presenters.Planets
 
         private readonly PlanetPopupView _view;
         private Planet _model;
+        private readonly MoneyStorage _moneyStorage;
 
-        public PlanetPopupPresenter(PlanetPopupView view)
+        public PlanetPopupPresenter(PlanetPopupView view, MoneyStorage moneyStorage)
         {
             _view = view;
+            _moneyStorage = moneyStorage;
         }
 
         public void Show(Planet model)
         {
             _model = model;
+            _model.OnUpgraded += UpgradedHandler;
             Invalidate();
             _view.SetVisible(true);
         }
@@ -30,17 +34,25 @@ namespace Game.Presenters.Planets
         void IInitializable.Initialize()
         {
             _view.OnClose += CloseHandler;
+            _moneyStorage.OnMoneyChanged += MoneyChangedHandler;
         }
 
         void IDisposable.Dispose()
         {
             _view.OnClose -= CloseHandler;
+            _moneyStorage.OnMoneyChanged -= MoneyChangedHandler;
+            if (_model == null) return;
+            _model.OnUpgraded -= UpgradedHandler;
         }
 
-        private void CloseHandler()
+        private void UpgradedHandler(int value)
         {
-            _view.SetVisible(false);
+            if (!_model.CanUpgrade) return;
+            _model.Upgrade();
+            Invalidate();
         }
+
+        private void CloseHandler() => _view.SetVisible(false);
 
         private void Invalidate()
         {
@@ -49,6 +61,10 @@ namespace Game.Presenters.Planets
             _view.SetPopulation($"Population: {_model.Population}");
             _view.SetIncome($"Income: {_model.MinuteIncome}/sec");
             _view.SetLevel($"Level: {_model.Level}/{_model.MaxLevel}");
+            _view.SetUpgradeEnabled(_model.CanUpgrade);
+            _view.SetUpgradePrice(_model.Price.ToString());
         }
+
+        private void MoneyChangedHandler(int newValue, int prevValue) => Invalidate();
     }
 }
--- Assets/Game/Scripts/Views/Planets/PlanetPopupView.cs
+++ Assets/Game/Scripts/Views/Planets/PlanetPopupView.cs
@@ -8,6 +8,7 @@ namespace Game.Views.Planets
     public class PlanetPopupView : MonoBehaviour
     {
         public event Action OnClose;
+        public event Action OnUpgrade;
 
         [SerializeField]
         private Image _icon;
@@ -21,26 +22,36 @@ namespace Game.Views.Planets
         private TMP_Text _income;
         [SerializeField]
         private Button _closeButton;
-        
+        [SerializeField]
+        private Button _upgradeButton;
+        [SerializeField]
+        private TMP_Text _upgradePrice;
+
         public void SetIcon(Sprite icon) => _icon.sprite = icon;
         public void SetTitle(string title) => _title.text = title;
 
         public void SetVisible(bool value) => gameObject.SetActive(value);
-        
+
         public void SetPopulation(string population) => _population.text = population;
         public void SetLevel(string level) => _level.text = level;
         public void SetIncome(string income) => _income.text = income;
 
+        public void SetUpgradeEnabled(bool value) => _upgradeButton.interactable = value;
+        public void SetUpgradePrice(string price) => _upgradePrice.text = price;
+
         private void OnEnable()
         {
             _closeButton.onClick.AddListener(CloseButtonClickHandler);
+            _upgradeButton.onClick.AddListener(UpgradeButtonClickHandler);
         }
 
         private void OnDisable()
         {
             _closeButton.onClick.RemoveListener(CloseButtonClickHandler);
+            _upgradeButton.onClick.RemoveListener(UpgradeButtonClickHandler);
         }
 
         private void CloseButtonClickHandler() => OnClose?.Invoke();
+        private void UpgradeButtonClickHandler() => OnUpgrade?.Invoke();
     }
 }
